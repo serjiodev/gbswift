dd3876dc96a660b97e0dda3de3f72d5dc3801cbe
Sergey Kosov <kosov1986@gmail.com>
2019-08-06 19:31:44 +0300

    add

--- VK offline app/LoginFormController.swift
+++ VK offline app/LoginFormController.swift
@@ -18,7 +18,8 @@ class LoginFormController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboadr))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,6 +42,33 @@ class LoginFormController: UIViewController {
         self.scrollView.scrollIndicatorInsets = insets
     }
     
+    //keyboard up
+    @objc func keyboardWasShow(notification: Notification) {
+        
+        //size keyboard
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue) .cgRectValue.size
+        _ = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        
+    }
+    
+    // keyboards hidden
+    @objc func keyboardWillBeHidden(notification: Notification){
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        
+        // padding bottom keyboard size
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    @objc func hideKeyboadr(){
+        self.scrollView?.endEditing(true)
+    }
+    
     
     @IBAction func loginButtonTap(_ sender: Any){
         let login = self.loginTextField.text ?? ""
@@ -52,6 +80,7 @@ class LoginFormController: UIViewController {
             print("authorization failed")
         }
     }
+    
 
 
 }
